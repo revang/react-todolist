fd1a5a829e7cce2007a3854394378a9899083f95
revang <revang.alternative@foxmail.com>
2022-06-24 13:54:49 +0800

    App调整为TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-function App() {
+function TodoList() {
   return (
     <div>
       hell, i am revang
@@ -6,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<TodoList />);
